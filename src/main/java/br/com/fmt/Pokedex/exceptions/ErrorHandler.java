5c82d98937079940066b2425158198535406c9cc
Felipe Quérette <felipe.querette@gmail.com>
2024-07-07 14:47:51 -0300

    implement error handling

--- src/main/java/br/com/fmt/Pokedex/exceptions/ErrorHandler.java
+++ src/main/java/br/com/fmt/Pokedex/exceptions/ErrorHandler.java
@@ -1,2 +1,54 @@
-package br.com.fmt.Pokedex.exceptions;public class ErrorHandler {
+package br.com.fmt.Pokedex.exceptions;
+
+import br.com.fmt.Pokedex.exceptions.dto.ErrorResponse;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Void> handleEntityNotFound(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateKey(DuplicateKeyException exception){
+        ErrorResponse error = new ErrorResponse();
+        error.setField("number");
+        error.setMessage(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleInadequateEnum(HttpMessageNotReadableException exception){
+        ErrorResponse error = new ErrorResponse();
+        error.setField("type");
+        error.setMessage(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorResponse>> handleValidationError(MethodArgumentNotValidException exception){
+        List<FieldError> fieldErrors = exception.getFieldErrors();
+
+        List<ErrorResponse> errors = fieldErrors.stream()
+                .map(ErrorResponse::new)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+
+    }
+
 }
--- src/main/java/br/com/fmt/Pokedex/exceptions/dto/ErrorResponse.java
+++ src/main/java/br/com/fmt/Pokedex/exceptions/dto/ErrorResponse.java
@@ -1,2 +1,32 @@
-package br.com.fmt.Pokedex.exceptions.dto;public class ErrorResponse {
+package br.com.fmt.Pokedex.exceptions.dto;
+
+import org.springframework.validation.FieldError;
+
+public class ErrorResponse {
+
+    private String field;
+    private String message;
+
+    public ErrorResponse(){}
+
+    public ErrorResponse(FieldError fieldError) {
+        this.field = fieldError.getField();
+        this.message = fieldError.getDefaultMessage();
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
